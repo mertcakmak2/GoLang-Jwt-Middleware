d04fd20cef5eaf92e2354cf6341f0645a5403979
mertcakmak <mertcakmak2@gmail.com>
2022-03-30 01:16:17 +0300

    password hash, login with jwt

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module middleware
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.7.7 // indirect
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-gonic/gin v1.7.7 // indirect
+	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
+)
--- main.go
+++ main.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
 	router := gin.Default()
-
-	router.GET("/middleware", ensureLoggedIn(), welcome)
-	router.GET("/without-middleware", welcome)
-
+	router.POST("/register", register)
+	router.POST("/login", login)
+	router.GET("/secure-welcome", authMiddleware(), welcome)
+	router.GET("/unsecure-welcome", welcome)
 	router.Run(":8091")
 }
 
@@ -21,17 +24,113 @@ func welcome(c *gin.Context) {
 	c.JSON(http.StatusOK, "welcome")
 }
 
-func ensureLoggedIn() gin.HandlerFunc {
+func login(c *gin.Context) {
+
+	fmt.Println(users)
+
+	var creds Credentials
+	if err := c.ShouldBindJSON(&creds); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	userHashPassword, isPresent := users[creds.Username]
+	if !isPresent {
+		c.JSON(http.StatusUnauthorized, "Wrong username or password")
+		return
+	}
+
+	if match := CheckPasswordHash(creds.Password, userHashPassword); !match {
+		c.JSON(http.StatusUnauthorized, "Wrong username or password")
+		return
+	}
+
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &Claims{
+		Username: creds.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, "error")
+		return
+	}
+
+	c.JSON(http.StatusOK, tokenString)
+}
+
+func register(c *gin.Context) {
+
+	var creds Credentials
+	if err := c.ShouldBindJSON(&creds); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	hash, _ := HashPassword(creds.Password)
+	users[creds.Username] = hash
+
+	creds.Password = hash
+	c.JSON(http.StatusCreated, creds)
+}
+
+func authMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authorization := c.GetHeader("Authorization")
-		jwt := strings.Split(authorization, "Bearer ")[1]
-
-		fmt.Println("ensure logged in middleware jwt :", jwt)
-
-		if jwt != "123" {
+		if authorization == "" || !strings.Contains(authorization, "Bearer ") {
 			c.JSON(http.StatusUnauthorized, gin.H{"Message": "Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		token := strings.Split(authorization, "Bearer ")[1]
+		claims := &Claims{}
+
+		tkn, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				c.JSON(http.StatusUnauthorized, "Authentication failed")
+				c.Abort()
+				return
+			}
+		}
+		if !tkn.Valid {
+			c.JSON(http.StatusUnauthorized, "Authentication failed")
 			c.Abort()
 			return
 		}
 	}
 }
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+var users = map[string]string{
+	// "user1": "password1",
+	// "user2": "password2",
+}
+
+var jwtKey = []byte("my_secret_key")
